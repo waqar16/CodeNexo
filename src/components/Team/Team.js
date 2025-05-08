4b5c915be2aa0f92b3462cc10da5df6004d16fdd
waqar16 <67152603+waqar16@users.noreply.github.com>
2025-05-08 17:28:45 +0500

    few changes

--- src/components/Team/Team.js
+++ src/components/Team/Team.js
@@ -19,7 +19,7 @@ function Team() {
                     </div>
                     <div className="col-xl-3 col-sm-6 mb-5">
                         <div className="bg-white rounded shadow-sm py-5 px-4 border">
-                            <img src="/Assets/Team/mubashir.jpg" alt="" width="100" className="img-fluid rounded-circle mb-3 img-thumbnail shadow-sm" />
+                            <img src="assets/Team/mubashir.jpg" alt="" width="100" className="img-fluid rounded-circle mb-3 img-thumbnail shadow-sm" />
                             <h5 className="mb-0">Mubashir Babar</h5>
                             <span className="small text-uppercase text-muted">Team Lead</span>
                             <ul className="social mb-0 list-inline mt-3">
@@ -32,7 +32,7 @@ function Team() {
                     </div>
                     <div className="col-xl-3 col-sm-6 mb-5" >
                         <div className="bg-white rounded shadow-sm py-5 px-4 border">
-                            <img src="/Assets/Team/hamza.jpg" alt="" width="100" className="img-fluid rounded-circle mb-3 img-thumbnail shadow-sm" />
+                            <img src="assets/Team/hamza.jpg" alt="" width="100" className="img-fluid rounded-circle mb-3 img-thumbnail shadow-sm" />
                             <h5 className="mb-0">M Hamza Khan</h5>
                             <span className="small text-uppercase text-muted">Co - Founder</span>
                             <ul className="social mb-0 list-inline mt-3">
@@ -45,7 +45,7 @@ function Team() {
                     </div>
                     <div className="col-xl-3 col-sm-6 mb-5">
                         <div className="bg-white rounded shadow-sm py-5 px-4 border">
-                            <img src="/Assets/Team/waqar.jpg" alt="" width="100" className="img-fluid rounded-circle mb-3 img-thumbnail shadow-sm" />
+                            <img src="assets/Team/waqar.jpg" alt="" width="100" className="img-fluid rounded-circle mb-3 img-thumbnail shadow-sm" />
                             <h5 className="mb-0">Waqar Khan</h5>    
                             <span className="small text-uppercase text-muted">Full Stack Developer</span>
                             <ul className="social mb-0 list-inline mt-3">
@@ -58,7 +58,7 @@ function Team() {
                     </div>
                     <div className="col-xl-3 col-sm-6 mb-5">
                         <div className="bg-white rounded shadow-sm py-5 px-4 border">
-                            <img src="/Assets/Team/amir.jpg" alt="" width="100" className="img-fluid rounded-circle mb-3 img-thumbnail shadow-sm" />
+                            <img src="assets/Team/amir.jpg" alt="" width="100" className="img-fluid rounded-circle mb-3 img-thumbnail shadow-sm" />
                             <h5 className="mb-0">Amir Ejaz</h5>
                             <span className="small text-uppercase text-muted">AI Engineer</span>
                             <ul className="social mb-0 list-inline mt-3">
@@ -71,7 +71,7 @@ function Team() {
                     </div>
                     <div className="col-xl-3 col-sm-6 mb-5">
                         <div className="bg-white rounded shadow-sm py-5 px-4 border">
-                            <img src="/Assets/Team/saim.jpg" alt="" width="100" className="img-fluid rounded-circle mb-3 img-thumbnail shadow-sm" />
+                            <img src="assets/Team/saim.jpg" alt="" width="100" className="img-fluid rounded-circle mb-3 img-thumbnail shadow-sm" />
                             <h5 className="mb-0">Saim Shehzad</h5>
                             <span className="small text-uppercase text-muted">Mern Stack Developer</span>
                             <ul className="social mb-0 list-inline mt-3">
